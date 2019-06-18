67398575b9b0da61cf8fce941e809b114b3cc948
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-06-19 09:08:17 +1000

    modules and packages added

---
+++ 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -0,0 +1,9 @@
+# import modules
+from mymodule import my_func
+# import packages
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+my_func()
+some_main_script.report_main()
+mysubscript.sub_report()
